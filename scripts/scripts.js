9e95a32e0e40c5d7873d14191eb2c69a537d685b
Gilles Kagarama <kagaramag@gmail.com>
2018-12-24 11:28:22 +0200

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,15 @@
 // switch nav bar
 function toggleSidebar() {
    var m = document.getElementById("sidebar");
+   var l = document.getElementById("layer");
    m.style.display = "block";
+   l.style.display = "block";
    
 }
 // close
 function closeSidebar() {
    var m = document.getElementById("sidebar");
+   var l = document.getElementById("layer");
    m.style.display = "none";
-   
+   l.style.display = "none";
 }
